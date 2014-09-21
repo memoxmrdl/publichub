c106b66b646b1d9da169d504b27111f20aa91e76
memoxmrdl <jmemox@gmail.com>
2014-09-20 21:38:10 -0500

    Remove erb to haml

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,8 +4,10 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-error alert-block"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
+    <div class="alert alert-danger alert-block alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert">
+      <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+    </button>
       #{messages}
     </div>
     HTML
