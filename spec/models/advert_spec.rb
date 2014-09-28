d86b4f155c75ea903640b866715b016bccee0837
memoxmrdl <jmemox@gmail.com>
2014-09-27 19:11:50 -0500

    Tests to Advert create a valid advert

--- spec/models/advert_spec.rb
+++ spec/models/advert_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Advert, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Advert do
+  it 'create a valid advert' do
+    advert = Advert.create do |a|
+      a.name = 'Nombre de prueba'
+      a.description = 'Este es un mensaje de prueba'
+      a.schedule = '9:00pm - 10:00pm'
+      a.phone = '3131054480'
+    end
+
+    expect(advert).to be_valid
+  end
 end
