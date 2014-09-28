a1deaf2dc57e536298f397677807533d86bbcefd
memoxmrdl <jmemox@gmail.com>
2014-09-27 19:01:07 -0500

    Move config to factory_girl file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -25,8 +26,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
-  confing.include FactoryGirl::Syntax::Methods
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
