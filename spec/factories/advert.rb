84be93d3969fd17e054ba1d8ce6f60c664be3557
memoxmrdl <jmemox@gmail.com>
2014-09-27 19:01:27 -0500

    Factories advert & user

---
+++ spec/factories/advert.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :advert do
+    name 'Tortas pachito'
+    description 'Ricas tortas de la region'
+    schedule '9:10am - 10:00pm'
+    phone '3131054480'
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email 'test@example.com'
+    password 'master19'
+  end
+end
